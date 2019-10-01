9d36c989c95cbbed091b071932553286edf25bb6
DazzaG <dazzag@gmail.com>
2019-10-01 21:42:29 +0100

    Added delete prior to create container

--- test.py
+++ test.py
@@ -9,16 +9,19 @@ load_dotenv(verbose=True)
 #mkdir ~/blob_emulator
 #docker run -e executable=blob -d -t -p 10000:10000 -v ~/blob_emulator:/opt/azurite/folder arafato/azurite
 
+#https://azure-storage.readthedocs.io/ref/azure.storage.blob.baseblobservice.html#azure.storage.blob.baseblobservice.BaseBlobService.list_blobs
+
 BLOB_ACCOUNT = os.environ['BLOB_ACCOUNT']
 BLOB_SAS = os.environ['BLOB_SAS']
-#CONTAINER="prefect-test"
-CONTAINER="devstoreaccount1"
+CONTAINER="prefect-test"
+#CONTAINER="devstoreaccount1"
 BLOB = "blob.json"
 DATA = json.dumps([1, 2, 3])
 
 #blob_service = blobsrv.BlockBlobService(account_name=BLOB_ACCOUNT, sas_token=BLOB_SAS)
 blob_service = asb.BlockBlobService(is_emulated=True)
 
+blob_service.delete_container(container_name=CONTAINER)
 blob_service.create_container(container_name=CONTAINER)
                 
 blob_service.create_blob_from_text(container_name=CONTAINER, blob_name=BLOB, text=DATA)
@@ -26,6 +29,8 @@ blob_service.create_blob_from_text(container_name=CONTAINER, blob_name=BLOB, tex
 blob_result = blob_service.get_blob_to_text(container_name=CONTAINER, blob_name=BLOB)
 
 blob_url = blob_service.make_blob_url(CONTAINER, BLOB)
+# this will look something like:
+# "https://{az_account_name}.blob.core.windows.net/{container}/{blob_name}"
 
 target_container = CONTAINER
 target_blob_name = "blob1.json"
@@ -36,7 +41,6 @@ blob_service.copy_blob(
     target_container,
     target_blob_name,
     blob_url
-    #f"https://{az_account_name}.blob.core.windows.net/{container}/{blob_name}"
 )
 
 for blob in blob_service.list_blobs(container_name=CONTAINER):
